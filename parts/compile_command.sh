3bad81b7d13ec691fde1b63f1e75c53c54696cc3
adazem009 <68537469+adazem009@users.noreply.github.com>
2021-02-03 15:00:33 +0100

    Update cpdisk command

--- parts/compile_command.sh
+++ parts/compile_command.sh
@@ -1103,8 +1103,8 @@ case "${command[0]}" in
 			abort_compiling "Number of arguments must be 2." 1 1
 		fi
 		process_argument ${command[1]}
-		if ((${#argument[@]} != 1)); then
-			abort_compiling "Number of inputs in the first argument must be 1." 1 10
+		if ((${#argument[@]} != 1)) && ((${#argument[@]} != 2)); then
+			abort_compiling "Number of inputs in the first argument must be 1 or 2." 1 10
 		fi
 		process_argument ${command[2]}
 		if ((${#argument[@]} != 1)); then
