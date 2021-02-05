56b5e7081aa9461a03f16870ec5bcc717ccffad2
adazem009 <68537469+adazem009@users.noreply.github.com>
2021-02-05 12:07:14 +0100

    Add readvar command

--- parts/compile_command.sh
+++ parts/compile_command.sh
@@ -1142,6 +1142,21 @@ case "${command[0]}" in
 		fi
 		echo "34/${command[1]}/${command[2]}" >> "./output/$FILE"
 		;;
+	"readvar")
+		# Read variable.
+		if ((${#command[@]} != 3)); then
+			abort_compiling "Number of arguments must be 2." 1 1
+		fi
+		process_argument ${command[1]}
+		if ((${#argument[@]} != 1)); then
+			abort_compiling "Number of inputs in the first argument must be 1." 1 10
+		fi
+		process_argument ${command[2]}
+		if ((${#argument[@]} != 1)); then
+			abort_compiling "Number of inputs in the first argument must be 1." 1 10
+		fi
+		echo "35/${command[1]}/${command[2]}" >> "./output/$FILE"
+		;;
 	"")
 		# Comment.
 		if [[ "$disout" != "1" ]]; then
