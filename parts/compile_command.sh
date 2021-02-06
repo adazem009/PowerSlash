c6091371f53f488a514b57d5b5bb445ce8b3f3e1
adazem009 <68537469+adazem009@users.noreply.github.com>
2021-02-06 12:07:47 +0100

    Add getindex command

--- parts/compile_command.sh
+++ parts/compile_command.sh
@@ -1157,6 +1157,21 @@ case "${command[0]}" in
 		fi
 		echo "35/${command[1]}/${command[2]}" >> "./output/$FILE"
 		;;
+	"getindex")
+		# Get index of an item.
+		if ((${#command[@]} != 3)); then
+			abort_compiling "Number of arguments must be 2." 1 1
+		fi
+		process_argument ${command[1]}
+		if ((${#argument[@]} != 2)); then
+			abort_compiling "Number of inputs in the first argument must be 2." 1 10
+		fi
+		process_argument ${command[2]}
+		if ((${#argument[@]} != 1)); then
+			abort_compiling "Number of inputs in the first argument must be 1." 1 10
+		fi
+		echo "36/${command[1]}/${command[2]}" >> "./output/$FILE"
+		;;
 	"")
 		# Comment.
 		if [[ "$disout" != "1" ]]; then
