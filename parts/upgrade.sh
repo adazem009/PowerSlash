b3dd199493b1c522d625ae90aaaf0542c587da15
adazem009 <68537469+adazem009@users.noreply.github.com>
2021-03-07 11:37:55 +0100

    Add linkable functions

--- parts/upgrade.sh
+++ parts/upgrade.sh
@@ -7,36 +7,50 @@
 # 1
 # 1
 # "Hello!"
+if [[ "$arch" = "" ]]; then
+	arch=smc
+fi
 if [[ "$arch" = "smc" ]]; then
 	if [[ "$1" != "1" ]]; then
 		echo "Converting to new SMC format..."
 	fi
+	if [[ "$2" = "" ]]; then
+		fname="$FILE"
+	else
+		fname="$2"
+	fi
 	# Rename output file
-	mv "./output/$FILE" "./output/${FILE}.old"
+	mv "./output/$fname" "./output/${fname}.old"
 	# Convert every line
-	IFS=$'\r\n' GLOBIGNORE='*' command eval  'out=($(cat "./output/${FILE}.old"))'
+	IFS=$'\r\n' GLOBIGNORE='*' command eval  'out=($(cat "./output/${fname}.old"))'
 	i=0
 	while (( i < ${#out[@]} )); do
 		i=$((i+1))
-		process_command "${out[$((i-1))]}"
-		echo "${command[0]}" >> "./output/$FILE"
-		echo "$((${#command[@]}-1))" >> "./output/$FILE"
-		i10=1
-		while (( i10 < ${#command[@]} )); do
-			i10=$((i10+1))
-			process_argument "${command[$((i10-1))]}"
-			echo "${#argument[@]}" >> "./output/$FILE"
-			i11=0
-			while (( i11 < ${#argument[@]} )); do
-				i11=$((i11+1))
-				echo "${argument[$((i11-1))]}" >> "./output/$FILE"
+		if [[ "${out[$((i-1))]}" = ">>" ]]; then
+			echo ">>" >> "./output/$fname"
+			echo "${out[$i]}" >> "./output/$fname"
+			echo "${out[$((i+1))]}" >> "./output/$fname"
+			i=$((i+2))
+		else
+			process_command "${out[$((i-1))]}"
+			echo "${command[0]}" >> "./output/$fname"
+			echo "$((${#command[@]}-1))" >> "./output/$fname"
+			i10=1
+			while (( i10 < ${#command[@]} )); do
+				i10=$((i10+1))
+				process_argument "${command[$((i10-1))]}"
+				echo "${#argument[@]}" >> "./output/$fname"
+				i11=0
+				while (( i11 < ${#argument[@]} )); do
+					i11=$((i11+1))
+					echo "${argument[$((i11-1))]}" >> "./output/$fname"
+				done
 			done
-		done
-		if [[ "$1" != "1" ]]; then
-			:
-			#echo -e "\e[1A\e[KConverting to new SMC format... line $i of ${#out[@]}..."
+			if [[ "$1" != "1" ]]; then
+				echo -e "\e[1A\e[KConverting to new SMC format... line $i of ${#out[@]}..."
+			fi
 		fi
 	done
 	# Remove old format file
-	rm "./output/${FILE}.old"
+	rm "./output/${fname}.old"
 fi
