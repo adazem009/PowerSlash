47e22ecc57bdc895dd1e57675900603609cdf8ab
adazem009 <68537469+adazem009@users.noreply.github.com>
2021-09-07 20:13:12 +0200

    Upgrade 'hidelogo' to SMC v3

--- pwc.c
+++ pwc.c
@@ -1458,7 +1458,7 @@ int main(int argc, char *argv[])
 			// hidelogo
 			if(cmd_argc != 0)
 				_error("Number of arguments must be 0",true,line+1,12,filename);
-			fprintf(ow,"29\n1\n1\n0\n");
+			fprintf(ow,"m5\n0\n0\n0\n0\n");
 		}
 		else if(strcmp(cmd,"enabletext") == 0)
 		{
